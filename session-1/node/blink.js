e5168b595a84947c7a82481667d6df2da3d1b6b6
nhat <nhat@hachr.com>
2013-07-17 03:58:04 +0000

    simplified the node blink

--- session-1/node/blink.js
+++ session-1/node/blink.js
@@ -1,29 +1,14 @@
 var LED = require('./led');
-var Q = require('q');
 
 var led = new LED(27);
 
 function blink(){
-	var defered = Q.defer();
 	led.turnOn(function(){
 		setTimeout(function(){
 			led.turnOff();
-			defered.resolve();
+			led.cleanup();
 		},200);
 	});
-	return defered.promise;
 }
 
-
-blink().then(function(){
-	return blink();
-}).then(function(){
-	return blink();
-});
-
-
-
-setTimeout(function(){
-	led.cleanup();
-},6000);
-
+blink();
