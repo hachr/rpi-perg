e30d4ad741ebb345c3548e028e99d21bde10b9c4
nhat <nhat@hachr.com>
2013-08-06 05:52:55 +0000

    add async stuff to led

--- session-2/node/led.js
+++ session-2/node/led.js
@@ -1,27 +1,44 @@
 var Gpio = require('onoff').Gpio;
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 
 
 
 function LED(pin){
 	this.pin = parseInt(pin,10);
 	this.gpio = new Gpio(pin,'out');
+	EventEmitter.call(this);
 }
 
+util.inherits(LED,EventEmitter);
 
 LED.prototype.turnOn = function(callback){
-	if(callback){
-		this.gpio.write(0, callback);
-	}else{
-		this.gpio.writeSync(0);
-	}
+	var self = this;
+	this.gpio.write(0, function(){
+		self.emit('on');
+		if(callback){
+			callback.call(null);
+		}
+	});
+};
+
+LED.prototype.turnOnSync = function(){
+	this.gpio.writeSync(1);
 };
 
 LED.prototype.turnOff = function(callback){
-	if(callback){
-		this.gpio.write(1,callback);
-	}else{
-		this.gpio.writeSync(1);
-	}
+	var self = this;
+	this.gpio.write(0, function(){
+		self.emit('off');
+		if(callback){
+			callback.call(null);
+		}
+	});
+	
+};
+
+LED.prototype.turnOffSync = function(){
+	this.gpio.writeSync(0);
 };
 
 LED.prototype.blink = function(blinkingDelay){
