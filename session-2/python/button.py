844e91f7fc16c16f5f569b089e1ea6d9f18ca555
nhat <nhat@hachr.com>
2013-08-06 23:52:59 +0000

    added button.py

---
+++ session-2/python/button.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+class Button:
+	"""Simple Button class"""
+
+	def __init__(self, pin):
+		self.pin = int(pin); #make sure it's a number.
+		if(self.pin > 30):
+			raise Exception("Cannot use pin higher than 30")
+		GPIO.setmode(GPIO.BCM) #switch mode
+		GPIO.setwarnings(False)
+		GPIO.setup(self.pin,GPIO.IN,initial=GPIO.LOW)
+
+
+	def waitUntilPress(self, interval = 0.2):
+		#loop and wait until button is pressed.
+		while GPIO.input(self.pin)==0:
+			self.wait(interval)
+		
+
+
+
+	def wait(self,interval):
+		time.sleep(interval)
+
+	#allow cleaning up
+	def cleanup(self):
+		GPIO.cleanup();
